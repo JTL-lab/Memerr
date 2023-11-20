be47403a814f191171a0cd13b7c76471de48e13d
Julia <juliatlewand@gmail.com>
2023-11-20 01:37:01 -0500

    Preliminary template for upload and search functionality

---
+++ frontend/script.js
@@ -0,0 +1,10 @@
+//
+// TODO:
+// - Replace the placeholder AWS SDK configuration,
+// - Replace function calls with actual implementation for AWS S3 uploads
+//
+<!-------------------------------------------- UPLOAD FUNCTIONALITY -------------------------------------------->
+
+<!--------------------------------------------- SEARCH FUNCTIONALITY -------------------------------------------->
+
+<!--------------------------------------------- GALLERY FUNCTIONALITY -------------------------------------------->
